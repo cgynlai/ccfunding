2a8d4f098a5e458b0f7c6e8333b501466835d050
cgynlai <cgynlai@gmail.com>
2022-02-15 23:05:38 +0800

    override configure() to permit login and static resource access

--- cf01-admin-parent/cf03-admin-component/src/main/java/com/cyl/crowd/mvc/config/WebAppSecurityConfig.java
+++ cf01-admin-parent/cf03-admin-component/src/main/java/com/cyl/crowd/mvc/config/WebAppSecurityConfig.java
@@ -1,6 +1,7 @@
 package com.cyl.crowd.mvc.config;
 
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 
@@ -8,4 +9,33 @@ import org.springframework.security.config.annotation.web.configuration.WebSecur
 @EnableWebSecurity
 public class WebAppSecurityConfig extends WebSecurityConfigurerAdapter {
 
+   @Override
+protected void configure(HttpSecurity security) throws Exception {
+	
+	security
+	   .authorizeRequests()
+	   .antMatchers("/admin/to/login/page.html")
+	   .permitAll()
+	   .antMatchers("/bootstrap/**")
+			   .permitAll()
+			   .antMatchers("/crowd/**")
+			   .permitAll()
+			   .antMatchers("/css/**")
+			   .permitAll()
+			   .antMatchers("/fonts/**")
+			   .permitAll()
+			   .antMatchers("/img/**")
+			   .permitAll()
+			   .antMatchers("/jquery/**")
+			   .permitAll()
+			   .antMatchers("/layer/**")
+			   .permitAll()
+			   .antMatchers("/script/**")
+			   .permitAll()
+			   .antMatchers("/ztree/**")
+			   .permitAll()
+	   .anyRequest()
+	   .authenticated();
+}
+   
 }
