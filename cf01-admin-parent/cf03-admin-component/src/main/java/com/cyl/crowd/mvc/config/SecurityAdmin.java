30d75932d4a12060779fb0dc61e0511dd9b4eec9
cgynlai <cgynlai@gmail.com>
2022-02-18 10:07:13 +0800

    added SecurityAdmin to encap admin object and auth list

--- cf01-admin-parent/cf03-admin-component/src/main/java/com/cyl/crowd/mvc/config/SecurityAdmin.java
+++ cf01-admin-parent/cf03-admin-component/src/main/java/com/cyl/crowd/mvc/config/SecurityAdmin.java
@@ -1,5 +1,38 @@
 package com.cyl.crowd.mvc.config;
 
-public class SecurityAdmin {
+import java.util.Collection;
+import java.util.List;
 
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.userdetails.User;
+
+import com.cyl.crowd.entity.Admin;
+
+/**
+ * User 对像仅包含帐号和密码，为了能方便地获取到原始地Admin对象，因此创建一个SecurityAdmin类，继承User。
+ * @author user
+ *
+ */
+
+public class SecurityAdmin extends User{
+	private static final long serialVersionUID = 1L;
+	
+	//原始的 Admin对像，包含 Admin对像的全部属性
+	private Admin originalAdmin;
+
+	public SecurityAdmin(Admin originalAdmin, List<GrantedAuthority> authorities) {
+		super(originalAdmin.getLoginAcct(), originalAdmin.getUserPswd() , authorities);
+		this.originalAdmin = originalAdmin;
+	}
+    //对外提供获取Admin对像的get方法。
+	public Admin getOriginalAdmin() {
+		return originalAdmin;
+	}
+
+	public void setOriginalAdmin(Admin originalAdmin) {
+		this.originalAdmin = originalAdmin;
+	}
+    
+	
+	
 }
